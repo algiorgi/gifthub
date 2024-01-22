d281fd0bbcfc704dcee7f979cd58fa61c321522d
Alejandro Giorgi <eze.giorgi@gmail.com>
2024-01-22 20:15:02 -0300

    Commit inicial

---
+++ app/root.jsx
@@ -0,0 +1,12 @@
+export default function App() {
+  return (
+    <html>
+      <head>
+        <link rel="icon" href="data:image/x-icon;base64,AA" />
+      </head>
+      <body>
+        <h1>Hello World</h1>
+      </body>
+    </html>
+  );
+}
