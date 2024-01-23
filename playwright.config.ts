7ce531d81977d602d467247be6666fc76b5fd068
Alejandro Giorgi <eze.giorgi@gmail.com>
2024-01-23 18:53:10 -0300

    Agrego un primer test basico con playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,7 +8,8 @@ export default defineConfig({
     workers: process.env.CI ? 1 : undefined,
     reporter: 'html',
     use: {
-        baseURL: 'http://127.0.0.1:3000'
+        baseURL: 'http://127.0.0.1:3000',
+        screenshot: 'only-on-failure',
     },
     projects: [
         {
@@ -20,5 +21,5 @@ export default defineConfig({
         command: 'npm run start',
         url: 'http://127.0.0.1:3000',
         reuseExistingServer: !process.env.CI,
-    }
+    },
 });
