0f99221274ff3463b72e85fef60dea5ad4b1423a
Alejandro Giorgi <eze.giorgi@gmail.com>
2024-01-24 16:44:52 -0300

    Borro yml e2e y corro las pruebas en el pipeline de ci

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,13 +17,10 @@ const config: PlaywrightTestConfig = {
             use: { ...devices['Desktop Chrome'] },
         },
     ],
-}
-
-if(!process.env.CI) {
-    config.webServer = {
+    webServer: {
         command: 'npm run start',
         url: 'http://127.0.0.1:3000',
-        reuseExistingServer: false,
+        reuseExistingServer: !process.env.CI
     }
 }
 
